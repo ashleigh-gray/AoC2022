33d23d000d9d06c6dfb2e0d081ce9a5a20508317
ashleighgray <ashleighgray@cloudsoft.io>
2022-12-04 12:02:23 +0000

    python - day 4 - parts 1 and 2

---
+++ day4/python/part2.py
@@ -0,0 +1,39 @@
+testing = False
+
+class elf_sections:
+    def __init__(self, input):
+        self.input = input
+        
+    def expand_range(self):
+        done = False
+        current = int(self.input.split('-')[0])
+        range = []
+        end = int(self.input.split('-')[1])
+        while done == False:
+            range.append(current)
+            current =+ current + 1 
+            if current > end:
+                done = True
+        self.range = range
+
+if testing == True:
+    filename = "../sample_input.txt"
+elif testing == False:
+    filename = "../input.txt"
+    
+input = open(filename, 'r')
+lines = input.readlines()
+
+total_intersections = []
+
+for line in lines:
+    elf1 = elf_sections(line.split(',')[0])
+    elf2 = elf_sections(line.split(',')[1])
+    elf1.expand_range()
+    elf2.expand_range()
+    if set(elf1.range).intersection(set(elf2.range)):
+        total_intersections.append(line.strip('\n'))
+    elif set(elf2.range).intersection(set(elf1.range)):
+        total_intersections.append(line.strip('\n'))
+        
+print("total intersections:", len(total_intersections))
